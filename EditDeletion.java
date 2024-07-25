32e3b319b3cd9306aeae86ce8286bd823f90da9d
Shota3312 <cy22283@shibaura-it.ac.jp>
2024-07-25 13:51:40 +0900

    Update EditDeletion.java

--- EditDeletion.java
+++ EditDeletion.java
@@ -1,8 +1,59 @@
 /**
  * EditDeletion
  * 要素の削除に責任を持つクラス
- * @author 
+ * @author Shota Kitagawa
  */
 
- import java.util.*;
  import java.sql.*;
+
+public abstract class EditDeletion {
+    public abstract String getSQLtemplate();
+    public abstract void setQuery(PreparedStatement st) throws SQLException;
+    public abstract void nullify(Connection conn) throws SQLException;
+    public abstract String showDetails(Connection conn) throws SQLException;    
+
+    public void preQuery() { } // 必要に応じてオーバーライド. 前処理がいらないならそのまま使う
+ 
+    public final void DBEdit() {
+        try {
+            // (必要なら)前処理 
+            preQuery();
+
+            // 接続
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost/keiba?useSSL=false&characterEncoding=utf8&useServerPrepStmts=true&zeroDateTimeBehavior=convertToNull", 
+                "root", ""
+            );		 
+            
+
+            String selectedDetails = showDetails(conn);
+
+            if (selectedDetails == null) {
+                conn.close();
+                return;
+            }
+
+            // SQLの前処理. getSQLtemplate() で個別のSQLを作る
+            PreparedStatement st = conn.prepareStatement(getSQLtemplate());
+             
+            // SQLに値を埋め込む. その他、個別に必要な処理もここで行う
+            setQuery(st);
+ 
+            // SQLを実行して結果を得る.
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Deleted " + rowsAffected + " row(s).");
+ 
+            
+            nullify(conn);
+            // 終了処理
+            st.close();
+            conn.close();
+        } catch (SQLException se) {
+            System.out.println("SQL Error: " + se.toString() + " "
+                + se.getErrorCode() + " " + se.getSQLState());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.toString() + e.getMessage());
+        }
+    }
+}
