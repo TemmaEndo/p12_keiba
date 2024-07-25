575107ab04010f3cf57cbc3bc53d8374c5be3e6a
cy22278 <cy22278@shibaura-it.ac.jp>
2024-07-25 13:11:41 +0900

    000

--- KeibaManager.java
+++ KeibaManager.java
@@ -8,7 +8,7 @@
  import java.util.*;
  import java.sql.*;
 
- public class KeibaManagerManager {
+ public class KeibaManager {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
