9f71f75e7e362deb176f227b9dbe22aea3cb7417
cy22278 <cy22278@shibaura-it.ac.jp>
2024-07-25 18:42:32 +0900

    000

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.sql.*;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        ChangeHorse sr = new ChangeHorse();
+        sr.DoChange();
+    }
+ 
+}
