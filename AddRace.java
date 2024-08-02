a731463ac7a7759e81d12989e565d7cf9029d46c
TemmaEndo <168059464+TemmaEndo@users.noreply.github.com>
2024-08-02 20:12:16 +0900

    Update AddRace.java
    ikesou?

--- AddRace.java
+++ AddRace.java
@@ -1,53 +1,55 @@
 /**
- * EditAddRace
+ * AddRace
  * レース要素の追加に責任を持つクラス
  * @author Temma Endo
  */
 
 import java.util.*;
 import java.sql.*;
+import java.io.*;
 
 public class AddRace extends Add{
-    /*
-    String sql1 = " INSERT INTO Race(ID, year, date, raceNum, name, baba, raceRank, num, distance) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
-    String sql2 = " SELECT MAX(race.ID) FROM race ";
+    
+    private Scanner scanner = new Scanner(System.in);
+
+    String sql1 = " INSERT INTO race(ID, year, date, raceNum, name, baba, raceRank, num, dist) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     @Override
-    void DbAdd(){
+    void DoAdd(){
         try{
             System.out.println("レース要素の入力: csvのパス");
             // ID(INT), 開催年(YEAR), 開催日(DATE), レース番号(INT), レース名(VARCHAR(20)), 馬場(VARCHAR(3)), レースランク(VARCHAR(2)), 頭数(INT), 距離(INT)
-           
-            rs = st.executeQuery(sql1);
-            int RaceID = rs.getInt("race.ID");
-            RaceID++;
+            
+            String filePass = scanner.nextLine();
 
-           //CSVからデータを受け取り、各変数に格納する。
+            File file = new File(filePass);
+            FileInputStream input = new FileInputStream(file);  
+            InputStreamReader stream = new InputStreamReader(input,"SJIS");
+            BufferedReader buffer = new BufferedReader(stream);
+       
+            String line;
+            
+            while ((line = buffer.readLine()) != null) {
+ 
+                byte[] b = line.getBytes();
+                line = new String(b, "UTF-8");
+                String race[] = line.split(",");
+                DBChange(sql1, race);
+            }
 
-            st.setInt(1, RaceID);
-
-            st.setYEAR(2, Year);
-            st.setDATE(3, Date);
-            st.setInt(4, RaceNum);
-            st.setString(5, Name);
-            st.setString(6, TrackCondition);
-            st.setString(7, RaceRank);
-            st.setInt(8, NumHorses);
-            st.setInt(9, Distance);
-
-            st.executeUpdate();
-        }catch (SQLException se) {
-            System.out.println("SQL Error 1: " + se.toString() + " "
-                + se.getErrorCode() + " " + se.getSQLState());
-        } catch (Exception e) {
+        }catch (Exception e) {
             System.out.println("Error: " + e.toString() + e.getMessage());
         }
     }
-*/
+
     protected void finalize() throws Throwable {
         // 終了処理
         rs.close();
         st.close();
         conn.close();
+    }
+
+    AddRace(){
+        super();
     }
 }
